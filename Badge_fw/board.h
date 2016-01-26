69d79284530bbad1ece0466418c4e601db882219
Kreyl <laystoll@yandex.ru>
2016-01-26 18:58:25 +0300

    + OS and UART with DMA

---
+++ Badge_fw/board.h
@@ -0,0 +1,117 @@
+/*
+ * board.h
+ *
+ *  Created on: 12 сент. 2015 г.
+ *      Author: Kreyl
+ */
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#include <inttypes.h>
+
+// ==== General ====
+#define BOARD_NAME          "Badge2"
+// MCU type as defined in the ST header.
+#define STM32F072xB
+
+// Freq of external crystal if any. Leave it here even if not used.
+#define CRYSTAL_FREQ_HZ     12000000
+
+#define SYS_TIM_CLK         (Clk.APBFreqHz * Clk.TimerClkMulti)
+
+#if 1 // ========================== GPIO =======================================
+// UART
+#define UART_GPIO       GPIOA
+#define UART_TX_PIN     9
+#define UART_RX_PIN     10
+#define UART_AF         AF1
+
+// LEDs
+#define LED_GPIO        GPIOB
+#define LED1_PIN        14
+#define LED2_PIN        15
+
+// USB
+#define USB_GPIO		GPIOA
+#define USB_DM_PIN		11
+#define USB_DP_PIN		12
+#define SNS_5V_GPIO     GPIOF
+#define SNS_5V_PIN      0
+#endif
+
+// Button
+#define BTN_GPIO        GPIOA
+#define BTN_PIN         0
+
+#if 1 // ========================= Timer =======================================
+#endif
+
+#if 1 // =========================== SPI =======================================
+#endif
+
+#if 1 // ========================== USART ======================================
+#define UART            USART1
+#define UART_TX_REG     UART->TDR
+#define UART_RX_REG     UART->RDR
+#endif
+
+#if 0 // ========================= Inner ADC ===================================
+#define ADC_REQUIRED        TRUE
+// Clock divider: clock is generated from the APB2
+#define ADC_CLK_DIVIDER		adcDiv2	// 32MHz / 2 = 16MHz
+
+// ADC channels: LED3, LED6
+#define SNS_CHNL0 	        10
+#define SNS_CHNL1 	        14
+
+#define ADC_VREFINT_CHNL    17  // All 4xx devices. Do not change.
+const uint8_t AdcChannels[] = { SNS_CHNL0, SNS_CHNL1 };
+#define ADC_CHANNEL_CNT     2   // Do not use countof(AdcChannels) as preprocessor does not know what is countof => cannot check
+#define ADC_SAMPLE_TIME     ast55d5Cycles
+#define ADC_SAMPLE_CNT      8   // How many times to measure every channel
+
+#define ADC_MAX_SEQ_LEN     16  // 1...16	// Const, see ref man
+#define ADC_SEQ_LEN         (ADC_SAMPLE_CNT * ADC_CHANNEL_CNT)
+#if (ADC_SEQ_LEN > ADC_MAX_SEQ_LEN) || (ADC_SEQ_LEN == 0)
+#error "Wrong ADC channel count and sample count"
+#endif
+#endif
+
+#if 1 // ========================== USB ========================================
+#define USBDrv          USBD1   // USB driver to use
+
+// CRS
+#define CRS_PRESCALER   RCC_CRS_SYNC_DIV1
+#define CRS_SOURCE      RCC_CRS_SYNC_SOURCE_USB
+#define CRS_POLARITY    RCC_CRS_SYNC_POLARITY_RISING
+#define CRS_RELOAD_VAL  ((48000000 / 1000) - 1) // Ftarget / Fsync - 1
+#define CRS_ERROR_LIMIT 34
+#define HSI48_CALIBRATN 32
+#endif
+
+#if 1 // =========================== DMA =======================================
+#define STM32_DMA_REQUIRED  TRUE
+// ==== Uart ====
+// Remap is made automatically if required
+#define UART_DMA_TX     STM32_DMA1_STREAM4
+#define UART_DMA_RX     STM32_DMA1_STREAM5
+#define UART_DMA_CHNL   0   // Dummy
+
+#if ADC_REQUIRED
+/* DMA request mapped on this DMA channel only if the corresponding remapping bit is cleared in the SYSCFG_CFGR1
+ * register. For more details, please refer to Section10.1.1: SYSCFG configuration register 1 (SYSCFG_CFGR1) on
+ * page173 */
+#define ADC_DMA         STM32_DMA1_STREAM1
+#define ADC_DMA_MODE    STM32_DMA_CR_CHSEL(0) |   /* DMA2 Stream4 Channel0 */ \
+                        DMA_PRIORITY_LOW | \
+                        STM32_DMA_CR_MSIZE_HWORD | \
+                        STM32_DMA_CR_PSIZE_HWORD | \
+                        STM32_DMA_CR_MINC |       /* Memory pointer increase */ \
+                        STM32_DMA_CR_DIR_P2M |    /* Direction is peripheral to memory */ \
+                        STM32_DMA_CR_TCIE         /* Enable Transmission Complete IRQ */
+#endif // ADC
+
+#endif
+
+#endif /* BOARD_H_ */
--- Badge_fw/main.cpp
+++ Badge_fw/main.cpp
@@ -1,149 +1,66 @@
 /*
- * File:   main.cpp
- * Author: Kreyl
- * Project: Armlet2South
+ * main.cpp
  *
- * Created on Feb 05, 2013, 20:27
+ *  Created on: 20 февр. 2014 г.
+ *      Author: g.kruglov
  */
 
-#include "kl_lib_f100.h"
-#include "ch.h"
-#include "hal.h"
-#include "cmd_uart_f10x.h"
-#include "cmd.h"
-#include "Sequences.h"
-#include "led_rgb.h"
+#include "main.h"
+//#include "SimpleSensors.h"
 
-// Button
-#define BTN_POLL_INTERVAL   18
-#define BTN_LONGPRESS_DELAY 3006
-#define BTN_GPIO            GPIOA
-#define BTN_PIN             0
-#define BtnPressedNow()     (PinIsSet(BTN_GPIO, BTN_PIN))
-
-#define THE_WORD            0xCa11  // For wake-up checking
-
-LedRGB_t Led({GPIOB, 0, TIM3, 3}, {GPIOB, 3, TIM2, 2}, {GPIOB, 1, TIM3, 4});
-
-enum State_t {stChanging, stStop, stSleep};
-State_t State = stChanging;
-
-// Prototypes
-static inline void GoSleep();
-static inline void LoadLedState();
-static inline void SaveLedState();
+App_t App;
 
 int main(void) {
-    // ==== Init clock system ====
-    Clk.SetupBusDividers(ahbDiv4, apbDiv1, apbDiv1);
+    // ==== Setup clock frequency ====
+//    Clk.EnablePrefetch();
+//    Clk.SetupBusDividers(ahbDiv2, apbDiv1);
     Clk.UpdateFreqValues();
-    // ==== Init OS ====
+
+    // Init OS
     halInit();
     chSysInit();
-    // ==== Init Hard & Soft ====
-    JtagDisable();
-    Uart.Init(115200);
-    Uart.Printf("\rBadge  AHB=%u; APB1=%u; APB2=%u\r\n", Clk.AHBFreqHz, Clk.APB1FreqHz, Clk.APB2FreqHz);
-    PinSetupIn(BTN_GPIO, BTN_PIN, pudNone); // Button
 
-    BackupRegs::Init();
+    // ==== Init hardware ====
+    Uart.Init(115200, UART_GPIO, UART_TX_PIN);//, UART_GPIO, UART_RX_PIN);
+    Uart.Printf("\r%S %S\r", APP_NAME, APP_VERSION);
 
-    Led.Init();
-    // Remap TIM2 CH2 to PB3
-    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-    uint32_t tmp = AFIO->MAPR;
-    tmp &= ~AFIO_MAPR_TIM2_REMAP;
-    tmp |= AFIO_MAPR_TIM2_REMAP_PARTIALREMAP1;  // 01: Partial remap (CH1/ETR/PA15, CH2/PB3, CH3/PA2, CH4/PA3)
-    AFIO->MAPR = tmp;
-    RCC->APB2ENR &= ~RCC_APB2ENR_AFIOEN;
+//    Clk.PrintFreqs();
 
-    uint32_t TimeStamp = 0;
-    bool BtnWasPressed = false, BtnIgnore = false;
+//    App.InitThread();
+//    PinSensors.Init();
 
-    // Check if just awake
-    if(BackupRegs::Read(1) == THE_WORD) {
-        Uart.Printf("\rWas in Standby");
-        // wait for a while checking button
-        TimeStamp = chTimeNow();
-        do {
-            chThdSleepMilliseconds(BTN_POLL_INTERVAL);
-            if(!BtnPressedNow()) GoSleep();
-        } while((chTimeNow() - TimeStamp) < BTN_LONGPRESS_DELAY);
-        BtnIgnore = true;
-        LoadLedState();
-    }
-    else Led.StartSequence(lsqChange);
+    // Main cycle
+    App.ITask();
+}
 
-    // Main cycle: btn polling
+__attribute__ ((__noreturn__))
+void App_t::ITask() {
     while(true) {
-        chThdSleepMilliseconds(BTN_POLL_INTERVAL);
-        // OnButtonPress
-        if(BtnPressedNow() and !BtnWasPressed) {
-            BtnWasPressed = true;
-            TimeStamp = chTimeNow();
-        }
-
-        // OnButtonRelease
-        else if(!BtnPressedNow() and BtnWasPressed) {
-            BtnWasPressed = false;
-            if(BtnIgnore) BtnIgnore = false;
-            else {
-                if(State == stChanging) {
-                    State = stStop;
-                    Led.PauseSequence();
-                }
-                else {
-                    State = stChanging;
-                    Led.ProceedPausedSequence();
-                }
-            }
-        }
-
-        // OnButtonLongPress
-        else if(BtnPressedNow() and ((chTimeNow() - TimeStamp) >= BTN_LONGPRESS_DELAY) and !BtnIgnore) {
-            // Shutdown lights and allow user to release button to minimize risk of inintended "keypress"
-            SaveLedState();
-            Led.StopAndOff();
-            chThdSleepMilliseconds(999);
-            GoSleep();
-        }
-    } // while
+        chThdSleepMilliseconds(207);
+        Uart.Printf("ogo\r");
+//        uint32_t EvtMsk = chEvtWaitAny(ALL_EVENTS);
+//        if(EvtMsk & EVTMSK_UART_NEW_CMD) {
+//            OnCmd((Shell_t*)&Uart);
+//            Uart.SignalCmdProcessed();
+//        }
+//#if 1 // ==== USB ====
+//        if(EvtMsk & EVTMSK_USB_READY) {
+//            Uart.Printf("UsbReady\r");
+//        }
+//#endif
+    } // while true
 }
 
-void GoSleep() {
-    chSysLock();
-    Sleep::EnableWakeupPin();
-    Sleep::EnterStandbyMode();
-    chSysUnlock();
+#if 1 // ======================= Command processing ============================
+void App_t::OnCmd(Shell_t *PShell) {
+	Cmd_t *PCmd = &PShell->Cmd;
+    __attribute__((unused)) int32_t dw32 = 0;  // May be unused in some configurations
+    Uart.Printf("%S\r", PCmd->Name);
+    // Handle command
+    if(PCmd->NameIs("Ping")) {
+        PShell->Ack(OK);
+    }
+
+    else PShell->Ack(CMD_UNKNOWN);
 }
-
-
-using namespace BackupRegs;
-void LoadLedState() {
-    State = State_t(Read(2));
-    Color_t Clr;
-    Clr.R = Read(3);
-    Clr.G = Read(4);
-    Clr.B = Read(5);
-    uint32_t CurrentChunk = Read(6);
-    Led.StartSequence(lsqChange, CurrentChunk);
-    if(State == stStop) Led.PauseSequence();
-    Led.SetColor(Clr);
-    Uart.Printf("\rLoad: St=%u  R=%u G=%u B=%u  Indx=%u", State, Clr.R, Clr.G, Clr.B, CurrentChunk);
-}
-
-void SaveLedState() {
-    AllowWrite();
-    Write(1, THE_WORD);
-    Write(2, (uint32_t)State);
-    Color_t Clr = Led.GetCurrentColor();
-    Write(3, Clr.R);
-    Write(4, Clr.G);
-    Write(5, Clr.B);
-    uint32_t CurrentChunk = Led.GetCurrentChunkIndx();
-    Write(6, CurrentChunk);
-    DenyWrite();
-//    Uart.Printf("\rSave: St=%u  R=%u G=%u B=%u  Indx=%u", State, Clr.R, Clr.G, Clr.B, CurrentChunk);
-//    chThdSleepMilliseconds(999);
-}
-
+#endif
